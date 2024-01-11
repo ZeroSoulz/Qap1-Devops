777eea271a07b915fddaecf7a5226b1c590bcf65
Alexander Harris <Alexander.Harris@keyin.com>
2024-01-11 09:16:48 -0330

    Three Commit

---
+++ src/test/java/com/keyin/SuggestionDatabaseTest.java
@@ -0,0 +1,18 @@
+package com.keyin;
+import java.util.HashMap;
+import java.util.Map;
+public class SuggestionDatabaseTest {
+    private Map<String, Integer> wordMap;
+
+    public Map<String, Integer> getWordMap(){
+        if (wordMap == null) {
+            wordMap = new HashMap<String, Integer>();
+        }
+
+        return wordMap;
+    }
+
+    public void setWordMap(Map<String, Integer> wordMap) {
+        this.wordMap = wordMap;
+    }
+}
